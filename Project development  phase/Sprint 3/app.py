fedd525e641a8f35d0afdb031842b8ac58cb6e06
Nisha0104 <95021630+Nisha0104@users.noreply.github.com>
2022-11-17 09:08:43 -0500

    Create app.py

---
+++ Project development  phase/Sprint 3/app.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle as p
+app=Flask(__name__)
+model=p.load(open('Ckd.pkl','rb'))
+@app.route('/')
+def HOME():
+    return render_template('home.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def prediction():    
+    form_value=request.form.values()    
+    data=[]
+    for x in form_value:
+        data.append(pd.to_numeric(x).astype(float))
+    features_value=[np.array(data)]  
+    features_name=['age','blood_urea','blood glucose random','coronary_artery_disease',
+                   'anemia','pus_cell','red_blood_cell','diabetesmellitus','pedal_edema']
+    df=pd.DataFrame(features_value, columns=features_name)
+    
+    output=model.predict(df)
+    if(output==0):
+        return render_template('result1.html')
+    else:
+        return render_template('result.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
